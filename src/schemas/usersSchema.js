83ee0db141c1fb72ccf8c8ca34e9398eb4857b48
yohan <yohan@yo.yo>
2023-01-24 16:13:37 -0300

    Fix: remodulate register schema to receive password confirmation

--- src/schemas/usersSchema.js
+++ src/schemas/usersSchema.js
@@ -4,7 +4,8 @@ const register = Joi.object({
     name: Joi.string().required(),
     roleName: Joi.string().min(1).required(),
     email: Joi.string().email({ tlds: { allow: false } }).required(),
-    password: Joi.string().required()
+    password: Joi.string().required(),
+    passwordConfirmation: Joi.string().required()
 })
 
 const loginCredentials = Joi.object({
