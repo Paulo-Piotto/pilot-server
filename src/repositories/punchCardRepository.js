5427f743eced4d4785a3b3362bd96a464d49850a
yohan <yohan@yo.yo>
2023-02-14 18:00:04 -0300

    Feat: implements filter queries to punch card data

--- src/repositories/punchCardRepository.js
+++ src/repositories/punchCardRepository.js
@@ -1,19 +1,99 @@
 import client from "../database.js";
 
-const config = {
-    employee: "employee_id",
-    client: "client_id",
-    date: "date"
+async function getAllPunchCardsData() {
+    const punchCardData = await client.employees_worked_days.findMany({
+        select: {
+            id: true, 
+            date: true,
+            clients: {
+                select: {
+                    id: true,
+                    name: true
+                }
+            },
+            employees: {
+                select: {
+                    id: true,
+                    name: true,
+                    wage: true
+                }
+            }
+        }
+    })
+    return punchCardData;
 }
 
-async function getPunchCardsGoupedBy(groupedBy) {
-    if(!config[groupedBy]) throw { type: "unprocessable_entity", message: `Could not group punchcard by: ${groupedBy}` }
-    const punchCardsByUsers = await client.employees_worked_days.groupBy({
-        by: [ config[groupedBy] ],
+async function getPunchCardsByClients(filter) {
+    const punchCardsByClients = await client.clients.findMany({
+        where: {
+            employees_worked_days: {
+                some: {
+                    date: {
+                        gte: filter.date?.from ?? new Date("1970").toISOString(),
+                        lte: filter.date?.to ?? new Date().toISOString()
+                    }
+                }
+            }
+        },
+        select: {
+            id: true,
+            name: true,
+            _count: {
+                select: {
+                    employees_worked_days: true // Sum of all workers worked days
+                }
+            }
+        }
     })
-    return punchCardsByUsers;
+    return punchCardsByClients;
+}
+
+async function gerPunchCardsByEmployees(filter) {
+    function whereClauseFactory() {
+        let dateFilter = { date: {} }
+        let clientFilter = {}
+
+        if(filter.client) clientFilter = {
+            clients: { name: filter.client },
+        }
+        if(filter.date?.from) dateFilter.date.gte = filter.date.from
+        if(filter.date?.to) dateFilter.date.lte = filter.date.to
+
+        return {
+            where: {
+                employees_worked_days: {
+                    some: { ...dateFilter, ...clientFilter }
+                }
+            }
+        }
+    }
+
+    const punchCardByEmployees = await client.employees.findMany({
+        ...(whereClauseFactory()),
+        select: {
+            id: true,
+            name: true,
+            wage: true,
+            start_day: true,
+            employees_worked_days: {
+                select: {
+                    id: true,
+                    date: true,
+                    clients: {
+                        select: {
+                            id: true,
+                            name: true,
+                        },
+                    }
+                }
+            }
+        }
+    })
+    return punchCardByEmployees;
 }
 
 export {
-    getPunchCardsGoupedBy
+    getAllPunchCardsData,
+    getPunchCardsByClients,
+    gerPunchCardsByEmployees
 }
--- src/services/punchCardService.js
+++ src/services/punchCardService.js
@@ -1,17 +1,8 @@
 import * as punchCardRepository from "../repositories/punchCardRepository.js"
 
 async function getPunchCardData() {
-    const requests = await Promise.all([
-       punchCardRepository.getPunchCardsGoupedBy("employee"),
-       punchCardRepository.getPunchCardsGoupedBy("client"),
-       punchCardRepository.getPunchCardsGoupedBy("date"),
-    ])
-
-    return {
-        employee: requests[0],
-        client: requests[1],
-        date: requests[2]
-    }
+    const punchCardsData = await punchCardRepository.gerPunchCardsByEmployees({})
+    return punchCardsData
 }
 
 export {
